908b2f1a196768e5123c1bc008c2bf1c0a3f26d5
akash <akash300383@gmail.com>
2022-11-27 20:26:04 +0530

    done

---
+++ src/main/java/com/MySampleController.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MySampleController {
+
+	@GetMapping(value = "/")
+	public String greeting() {
+		return "Welcome to Spring boot with AWS created by Akash Kale!";
+	}
+	
+	@GetMapping(value = "say/{name}")
+	public String sayHello(@PathVariable("name") String name) {
+		return "Welcome to Spring boot with AWS by user "+name;
+	}
+}
---
+++ src/main/java/com/SpringBootWithAwsJenkinDockerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithAwsJenkinDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithAwsJenkinDockerApplication.class, args);
+	}
+
+}
